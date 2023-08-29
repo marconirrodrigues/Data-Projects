0be33cf67cc5052ecc066cacf5bf3dd19fc65d95
Marconi Rodrigues <marconirodrigues.data@gmail.com>
2023-08-29 15:20:02 -0300

    Reestruturação de pastas

--- Investiment-DataOps-Project/tests/test_database_manager.py
+++ Investiment-DataOps-Project/tests/test_database_manager.py
@@ -1,33 +1,21 @@
-import unittest
 import sqlite3
-from src.database_manager import DatabaseManager
+import pytest
+from src.database_manager import DatabaseManagerForTest
 
-# Classe para extensão do comportamento no teste
-class DatabaseManagerForTest(DatabaseManager):
-    def __init__(self, conn):
-        self.conn = conn
-        self.conn.isolation_level = None  # Para ativar o autocommit no SQLite
-        self.cursor = self.conn.cursor()
+@pytest.fixture
+def db_manager():
+    conn = sqlite3.connect(':memory:')  # Criar um banco de dados SQLite na memória
+    db_manager = DatabaseManagerForTest(conn)
+    yield db_manager  # yield funciona como return, mas garante que o código após seja executado ao final
+    conn.close()
 
-class TestDatabaseManager(unittest.TestCase):
-
-    def setUp(self):
-        self.conn = sqlite3.connect(':memory:')  # Criar um banco de dados SQLite na memória
-        self.db_manager = DatabaseManagerForTest(self.conn)
-
-    def tearDown(self):
-        self.conn.close()
-
-    def test_insert_data(self):
-        create_table_query = """CREATE TABLE IF NOT EXISTS stocks
-                                (date text, trans text, symbol text,
-                                 qty real, price real)"""
-        self.db_manager.create_table(create_table_query)
-        data = ('2023-10-05', 'BUY', 'RHAT', 100, 35.14)
-        insert_query = """INSERT INTO stocks VALUES (?, ?, ?, ?, ?)"""
-        self.db_manager.insert_data(insert_query, data)
-        self.db_manager.cursor.execute('SELECT * FROM stocks')
-        self.assertEqual(self.db_manager.cursor.fetchone(), data)
-
-if __name__ == '__main__':
-    unittest.main()
+def test_insert_data(db_manager):
+    create_table_query = """CREATE TABLE IF NOT EXISTS stocks
+                            (date text, trans text, symbol text,
+                             qty real, price real)"""
+    db_manager.create_table(create_table_query)
+    data = ('2023-10-05', 'BUY', 'RHAT', 100, 35.14)
+    insert_query = """INSERT INTO stocks VALUES (?, ?, ?, ?, ?)"""
+    db_manager.insert_data(insert_query, data)
+    db_manager.cursor.execute('SELECT * FROM stocks')
+    assert db_manager.cursor.fetchone() == data
